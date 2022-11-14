4ed156d86b640915d8f754264d806dcdd080638e
Tehos <postolacheteodor@gmail.com>
2022-11-14 15:47:25 +0200

    Add pad resizing feature

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const container = document.querySelector(".container");
-container.setAttribute("draggable", false);
-function createCell() {
-  const newDiv = document.createElement("div");
-  newDiv.classList.add("cell");
-  container.appendChild(newDiv);
+const scaleBtn = document.querySelector(".scale");
+
+// CREATE INITIAL PAD
+
+let userInput = 32;
+
+function createCells(cells) {
+  container.setAttribute(
+    "style",
+    `grid-template-columns: repeat(${cells}, ${480 / cells}px);
+    grid-template-rows: repeat(${cells}, ${480 / cells}px)`
+  );
+  for (let i = 0; i < cells ** 2; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("cell");
+    container.appendChild(newDiv);
+  }
 }
 
-for (i = 0; i < 1024; i++) {
-  createCell();
-}
-const gridCells = document.querySelectorAll(".cell");
+createCells(userInput);
+let gridCells = document.querySelectorAll(".cell");
 
+// DRAWING EVENT LISTENERS
 let toggleMouseDown = false;
 
 gridCells.forEach((cell) => {
@@ -26,9 +37,51 @@ gridCells.forEach((cell) => {
     }
   });
 });
+
 gridCells.forEach((cell) => {
-  cell.addEventListener("mouseup", function () {
-    toggleMouseDown = false;
-    console.log(toggleMouseDown);
+  cell.addEventListener("click", function (event) {
+    event.target.style.backgroundColor = "#333";
   });
+});
+
+window.addEventListener("mouseup", function () {
+  toggleMouseDown = false;
+  console.log(toggleMouseDown);
+});
+
+// SCALE THE PAD
+
+scaleBtn.addEventListener("click", function () {
+  let userInput = prompt("Choose size of the sketch pad between 16 and 100:");
+  if (userInput < 16 || userInput > 100) {
+    alert("The side lenght of the pad must be between 16 and 100!");
+  } else {
+    container.innerHTML = "";
+    createCells(Number(userInput));
+    let newCells = document.querySelectorAll(".cell");
+    newCells.forEach((cell) => {
+      cell.addEventListener("mousedown", function () {
+        toggleMouseDown = true;
+        console.log(toggleMouseDown);
+      });
+    });
+    newCells.forEach((cell) => {
+      cell.addEventListener("mouseenter", function (event) {
+        if (toggleMouseDown) {
+          event.target.style.backgroundColor = "#333";
+        }
+      });
+    });
+
+    newCells.forEach((cell) => {
+      cell.addEventListener("click", function (event) {
+        event.target.style.backgroundColor = "#333";
+      });
+    });
+
+    window.addEventListener("mouseup", function () {
+      toggleMouseDown = false;
+      console.log(toggleMouseDown);
+    });
+  }
 });
